2f34aa9526c80b01214cfa53b3fc7c45a6babba0
Obby <obbydelrosario@gmail.com>
2022-12-08 19:30:17 -0800

    updated code to add new blog posts

--- models.py
+++ models.py
@@ -15,7 +15,7 @@ class User(db.Model):
     """User"""
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     first_name = db.Column(db.String(50), nullable = False, unique= False)
     last_name = db.Column(db.String(50), nullable = False, unique= False)
     # image_url = db.Column(db.String(300), nullable = False, unique= False default = "https://i.stack.imgur.com/34AD2.jpg")
@@ -35,12 +35,12 @@ class Post(db.Model):
     """Post Model"""
     __tablename__ = 'posts'
 
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     title = db.Column(db.Text, nullable = True, unique = False)
     content = db.Column(db.Text, nullable = True, unique = False)
     created_at = db.Column(db.DateTime(timezone=True), nullable = True, server_default=func.now())
     # time_created = Column(DateTime(timezone=True), server_default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     user = db.relationship('User', backref = 'posts')
     
     def __refr__(self):
--- test_flask.py
+++ test_flask.py
@@ -8,7 +8,7 @@ from models import db, connect_db, User, Post
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test_db'
-app.config['SQLALCHEMY_ECHO'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 
 app.config['SECRET_KEY'] = "SECRET!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS']= False
@@ -37,9 +37,15 @@ app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 #     db.create_all()
 
 # below or from flask import fixed the testcase overriding main database
+# below code working at the moment and not overriding original db
+# test db data not getting deleted after test though
 connect_db(app)
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.drop_all()
+#     db.create_all()
+
+db.drop_all()
+db.create_all()
 
 app.app_context().push()
 
@@ -63,6 +69,7 @@ class UserViewsTestCase(TestCase):
         """Clean up any fouled transaction."""
         with app.app_context():
             db.session.rollback()
+
             # BELOW BREAKS TEST            
             # db.drop_all()
 
@@ -110,6 +117,109 @@ class UserViewsTestCase(TestCase):
             
             self.assertEqual(resp.status_code, 404)
 
+    def test_blog_page(self):
+        """test if posting a blog works"""
+        with app.test_client() as client:
+            # resp = client.get(f"/users/{user.id}/posts/new", data=d, follow_redirects=True)
+            resp = client.get("/posts")
+            self.assertEqual(resp.status_code, 200)
+
+    def test_add_new_post(self):
+        with app.test_client() as client:
+            """test adding a new post"""
+            user = User.query.get_or_404(self.user_id)
+            post = {"title": "test title", "content": "test content",
+             "user": user }
+            #problem below
+            # resp = client.post(f"/users/{self.user_id}/posts/new", data=post, follow_redirects=True)
+            
+            
+    # resp = client.post(f'/users/{self.user_id}/posts/new', data={'title': 'Test title', 'content': 'Test stuff', 'user_id': self.user_id})
+
+    # def test_add_new_post(self):
+        # with app.test_client() as client:
+            # user was already made with set up file above for every new test
+            # resp = client.post(f'/users/{self.user_id}/posts/new', 
+			# data={'title': 'Test title', 'content': 'Test stuff', 'user_id': self.user_id})
+            # user = User.query.get(self.user_id)
+            # post = Post.query.filter_by(title = 'Test title').first()
+			# Redirect to check if post got posted.
+            # html = client.get(f'/posts/{post.id}').get_data(as_text=True)
+            # self.assertIn('Test title', html)
+            # self.assertIn('Test stuff', html)
+            # self.assertIn(user.full_name, html)   
+
+
+# @app.route("/users/<int:user_id>/posts/new", methods=["POST"])
+# def post_post(user_id):
+#     """post the new post"""
+#     title = request.form["title"]
+#     content = request.form["content"]
+#     user = User.query.get_or_404(user_id)
+#     new_post = Post(title=title, content = content, user = user)
+#     db.session.add(new_post)
+#     db.session.commit()
+#     # return redirect(f"/posts/{new_post.id}")
+#     return redirect(f"/posts/{new_post.id}")
+
+#  def test_add_user(self):
+#         """test that when new user added with first_name of TestFirstName it shows up properly on next page"""
+#         with app.test_client() as client:
+#             # note first-name was used instead of first_name as below data is posted to the form
+#             # form fields are looking for name="first-name", "last-name", "image-url" NOT first_name, last_name, image_url
+#             d = {"first-name": "TestFirstName", "last-name": "TestLastName", "image-url": "www.google.com"}
+#             resp = client.post("/users/new", data=d, follow_redirects=True)
+#             html = resp.get_data(as_text=True)
+#             # if didnt follow redirects it would be status code 301/302
+#             self.assertEqual(resp.status_code, 200)
+#             self.assertIn("<h1>TestFirstName Details</h1>", html)
+
+# broken stuff:
+
+
+    # def test_add_blog(self):
+    #     """test add blog to test db"""
+    #     with app.test_client() as client:
+    #         # user = User.query.filter_by(first_name = 'TestUser_firstname').first()
+    #         # user = User.query.get_or_404(user_id)
+    #         # userid = 
+    #         # search test user and get user id.
+    #         # use that id to make test post with f string below
+    #         # need to include user data in posted object. Not part of html form but IS part of SQL query
+    #         # d = {"title": "test-title", "content": "test-content", "user_id": user}
+    #         # d = {"title": "test-title", "content": "test-content", "user_id": 6}
+    #         # resp = client.post(f"/users/{user.id}/posts/new", data = d, follow_redirects= True)
+    #         user_obj = User.query.filter_by(first_name = 'TestUser_firstname').first()
+    #         user_id = user_obj.id
+    #         user = User.query.get_or_404(user_id)
+    #         d = {"title": "test-title", "content": "test-content", "user": user}
+    #         # resp = client.post("/users/6/posts/new", data = d, follow_redirects= True)
+    #         resp = client.post(f"/users/{user_id}/posts/new", data = d, follow_redirects= True)
+
+    #         # html = resp.get_data(as_text=True)
+    #         # resp = client.get(f"/posts")
+    #         self.assertEqual(resp.status_code, 200)
+          
+    # resp = client.post(f"/users/{user.id}/posts/new", data=d, follow_redirects=True)
+    # def test_blogposts(self):
+    #     """test if posting a blog works. first add user, then post a blog with user"""
+    #     with app.test_client() as client:
+    #         # resp = client.get(f"/users/{user.id}/posts/new", data=d, follow_redirects=True)
+    #         # resp = client.post(f"/users/{post.user.id}/posts/new", )
+    #         # self.assertEqual(resp.status_code, 200)
+    #         d = {"first-name": "TestFirstName", "last-name": "TestLastName", "image-url": "www.google.com"}
+    #         resp = client.post("/users/new", data=d, follow_redirects=True)
+    #         html = resp.get_data(as_text=True)
+    #         # if didnt follow redirects it would be status code 301/302
+    #         self.assertEqual(resp.status_code, 200)
+    #         self.assertIn("<h1>TestFirstName Details</h1>", html)
+    #         # user = User.query.filter_by(first_name = 'TestUser_firstname').first()
+    #         d2 = {"title": "test-title", "content": "test-content"}
+    #         resp = client.post("/users/10/posts/new", data = d2, follow_redirects= True)
+    #         self.assertEqual(resp.status_code, 200)
+    #         # resp = client.post(f"/users/{user.id}/posts/new", data=d, follow_redirects=True)   
+
+            
 
     # 1. Add test if user visits incorrect page id (none existent pet)
     # 2. Add test if user tries to add duplicate pet
